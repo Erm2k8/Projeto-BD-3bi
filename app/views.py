528b29decc483399020104b618d00f830f62a78e
Ermesson Andrade Belarmino <20231011110017@ifrn.local>
2025-09-19 14:30:32 -0300

    exercicio 01

---
+++ app/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+
+import pyodbc
+from .util_conexao import obter_conexao, executar_select, executar_sql
+
+
+def home(request):
+    # define a página HTML (template) que deverá será carregada
+    template = 'home.html'
+    return render(request, template)
+
+
+def dados_gerais(request):
+
+    # define a página HTML (template) que deverá será carregada
+    template = 'dados_gerais.html'
+    try:
+        # obtem a conexao com o BD
+        conexao = obter_conexao()
+
+        # define um cursor para executar comandos SQL
+        cursor = conexao.cursor()
+
+        # obtem a quantidade de registros de Instituicoes Financeiras
+        sql = 'SELECT count(*) FROM IES '
+        # obtem o valor retornado usando "fetchval"
+        count_ies = cursor.execute(sql).fetchval()
+
+        # obtem a quantidade de registros de Cursos
+        sql = 'SELECT count(*) FROM campus '
+        count_campus = cursor.execute(sql).fetchval()
+
+        # obtem a quantidade de registros de Cursos
+        sql = 'SELECT count(*) FROM curso '
+        count_cursos = cursor.execute(sql).fetchval()
+
+        # obtem a quantidade de registros de Cursos
+        sql = 'SELECT count(*) FROM cursos_oferecidos_por_campus '
+        count_ofertas = cursor.execute(sql).fetchval()
+
+        # obtem a quantidade de registros de Cursos
+        sql = 'SELECT count(*) FROM area '
+        count_areas = cursor.execute(sql).fetchval()
+
+        # define a pagina a ser carregada, adicionando os registros das tabelas 
+        return render(request, template, 
+                    context={
+                          'count_ies': count_ies,
+                          'count_campus': count_campus,
+                          'count_cursos': count_cursos,
+                          'count_ofertas': count_ofertas,
+                          'count_areas': count_areas,
+                    })
+    
+    # se ocorreu algunm erro, insere a mensagem para ser exibida no contexto da página 
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
+
+
+def qtd_campi_uf(request):
+    template = 'campi_uf.html'
+
+    try:
+        conn = obter_conexao()
+        cursor = conn.cursor()
+
+        sql = """
+            SELECT Municipio.uf, COUNT(*) FROM Campus
+            INNER JOIN Municipio ON Campus.id_municipio = Municipio.id_municipio 
+            GROUP BY Municipio.uf
+            ORDER BY Municipio.uf
+        """
+
+      
+        result = cursor.execute(sql).fetchall()
+
+
+        print(result)
+
+        return render(request, template, 
+                    context={
+                          'result': result,
+                    })
+
+    except Exception as err:
+        return render(request, template, context={'ERRO': err})
